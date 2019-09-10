dfb574c3310d18021ca837b1c61640e5de4a5d92
ccuulinay <ccuulinay@gmail.com>
2019-09-10 16:22:25 +0800

    feat: upgrade downloader to support pickup from exception

--- spiders/images_downloader.py
+++ spiders/images_downloader.py
@@ -60,11 +60,6 @@ def levelx_processing(level_cat, cur_pattern=None, samples=[]):
         return samples
 
 
-lv_names = []       
-for i in SH_GARBAGE_CLS_CAT:
-    x = levelx_processing(i)
-    if x:
-        lv_names.extend(x)
 
 
 def google_target_images(query_kw):
@@ -111,19 +106,90 @@ def google_download_target_images(image_url, image_format, save_file_prefix, sav
     else:
         raise Exception("Error when download image.")
 
+## Below begin to work.
+
+
+lv_names = []
+for i in SH_GARBAGE_CLS_CAT:
+    x = levelx_processing(i)
+    if x:
+        lv_names.extend(x)
+
+
+
+save_path = "../data"
+except_temp_file = "./temp_state.ind"
+
+if os.path.exists(except_temp_file):
+    with open(except_temp_file, "r") as f:
+        lasttime_save_file_name = f.readline()
+        lasttime_download_num = f.readline()
+
+    last_level = lasttime_save_file_name.replace("\n", "").split("_")
+    last_level_index = lv_names.index(last_level)
+
+    print(last_level_index)
 
 count = 0
 sleep_interval = 10
-for i in lv_names[:]:
+
+if last_level_index and int(lasttime_download_num) >= 80:
+    start_level = last_level_index + 1
+
+end_level = len(lv_names) + 1
+
+start_image = 0
+max_end_image = 100
+
+if last_level_index and int(lasttime_download_num) < 80:
+    i = lv_names[last_level_index]
+    query_sample = i[-1]
+    save_file_name = "_".join(i)
+    target_images = google_target_images(query_sample)
+    temp_end_image = min(max_end_image, len(target_images))
+    for image_url, image_format in target_images[int(lasttime_download_num):temp_end_image]:
+        try:
+            google_download_target_images(image_url, image_format, save_file_name, save_path=save_path)
+        except:
+            with open(except_temp_file, "w") as f:
+                f.write(save_file_name)
+                current_num = len(glob.glob(save_path + "/" + save_file_name + "*"))
+                f.write("\n")
+                f.write(str(current_num))
+        # Counter number of request, sleep few seconds every few requests.
+        count += 1
+        if count % sleep_interval == 0:
+            time.sleep(3)
+    with open(except_temp_file, "w") as f:
+        f.write(save_file_name)
+        current_num = len(glob.glob(save_path + "/" + save_file_name + "*"))
+        f.write("\n")
+        f.write(str(current_num))
+
+    start_level = last_level_index + 1
+
+for i in lv_names[start_level:end_level]:
     query_sample = i[-1]
     save_file_name = "_".join(i)
     target_images = google_target_images(query_sample)
     logging.warning(len(target_images))
-    for image_url, image_format in target_images[:]:
-        google_download_target_images(image_url, image_format, save_file_name, save_path="../data")
-        # Counter number of request, sleep 5 seconds every 50 requests.
+    temp_end_image = min(max_end_image, len(target_images))
+    for image_url, image_format in target_images[start_image:temp_end_image]:
+        try:
+            google_download_target_images(image_url, image_format, save_file_name, save_path=save_path)
+        except:
+            with open(except_temp_file, "w") as f:
+                f.write(save_file_name)
+                current_num = len(glob.glob(save_path + "/" + save_file_name + "*"))
+                f.write("\n")
+                f.write(str(current_num))
+        # Counter number of request, sleep few seconds every few requests.
         count += 1
         if count % sleep_interval == 0:
             time.sleep(3)
-
+    with open(except_temp_file, "w") as f:
+        f.write(save_file_name)
+        current_num = len(glob.glob(save_path + "/" + save_file_name + "*"))
+        f.write("\n")
+        f.write(str(current_num))
 
