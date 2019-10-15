fd05bcce4332bae2137fc680b3506853da9332ef
ccuulinay <ccuulinay@gmail.com>
2019-10-15 20:01:06 +0800

    feat: add static pages

---
+++ gc_api/static/precache-manifest.c741e835e1b62fc274500cafcef11740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7388013499d70d55d5086a6226f4ae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d803eb9734944d92b3f",
+    "url": "/static/css/2.5a13bb67.chunk.css"
+  },
+  {
+    "revision": "377892d47e6798c33daa",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0d803eb9734944d92b3f",
+    "url": "/static/js/2.4ceaa5b2.chunk.js"
+  },
+  {
+    "revision": "377892d47e6798c33daa",
+    "url": "/static/js/main.a5d779c8.chunk.js"
+  },
+  {
+    "revision": "ea03d3ddf5ada1019748",
+    "url": "/static/js/runtime-main.2e7eae06.js"
+  }
+]);
